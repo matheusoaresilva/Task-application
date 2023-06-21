eea9eb7197e6e63a284bf3e91ad84bb0fa7d793b
Matheus Soares <matheusmatias2622@gmail.com>
2023-06-20 23:14:09 -0300

    add front end

---
+++ front-end/js/buscarTarefa.js
@@ -0,0 +1,57 @@
+// Função para buscar todas as tarefas do back-end
+function buscarTarefas() {
+  axios
+    .get("http://localhost:8080/tasks/all")
+    .then((response) => {
+      const data = response.data;
+      const container = document.getElementById("container-tarefas");
+      container.innerHTML = ""; // Limpar o container
+ 
+      data.forEach((tarefa) => {
+        const cardDiv = document.createElement("div");
+        cardDiv.classList.add("col-md-3", "custom-margin");
+
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const cardHeader = document.createElement("div");
+        cardHeader.classList.add("card-header");
+        cardHeader.textContent = tarefa.categoria;
+
+        const cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+
+        const cardTitle = document.createElement("h5");
+        cardTitle.classList.add("card-title");
+        cardTitle.textContent = tarefa.titulo;
+
+        const cardText = document.createElement("p");
+        cardText.classList.add("card-text");
+        cardText.textContent = tarefa.descricao;
+
+        const button = document.createElement("a");
+        button.href = "#";
+        button.classList.add("btn", "btn-primary");
+        button.textContent = "Ed";
+
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardText);
+        cardBody.appendChild(button);
+
+        card.appendChild(cardHeader);
+        card.appendChild(cardBody);
+
+        cardDiv.appendChild(card);
+
+        container.appendChild(cardDiv);
+      });
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar tarefas:", error);
+    });
+}
+
+// Evento de carregamento da página
+window.onload = function () {
+  buscarTarefas();
+};
---
+++ front-end/js/form.js
@@ -0,0 +1,61 @@
+// Função para exibir o formulário flutuante e a sobreposição escura
+function showFloatingContainer() {
+  document.getElementById("floatingContainer").classList.add("show");
+  document.querySelector(".dark-overlay").style.display = "block";
+}
+
+// Função para fechar o formulário flutuante e remover a sobreposição escura
+function closeFloatingContainer() {
+  document.getElementById("floatingContainer").classList.remove("show");
+  document.querySelector(".dark-overlay").style.display = "none";
+}
+
+// Evento de clique no botão "New task"
+document
+  .getElementById("newTaskButton")
+  .addEventListener("click", showFloatingContainer);
+
+// Evento de clique no botão "Fechar"
+document
+  .getElementById("closeButton")
+  .addEventListener("click", closeFloatingContainer);
+
+// Enviar os dados do formulário para o back-end ao salvar
+const newTaskForm = document.getElementById("newTaskForm");
+
+newTaskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const tituloInput = document.getElementById("tituloInput").value;
+  const descricaoInput = document.getElementById("descricaoInput").value;
+  const categoriaInput = document.getElementById("categoriaInput").value;
+  const statusInput = document.getElementById("statusInput").value;
+
+  const taskData = {
+    titulo: tituloInput,
+    descricao: descricaoInput,
+    categoria: categoriaInput,
+    status: statusInput,
+  };
+
+  axios
+    .post("http://localhost:8080/tasks/save", taskData)
+    .then((response) => {
+      // Verificar o status da resposta
+      if (response.status === 201) {
+        // Limpar os campos do formulário
+        newTaskForm.reset();
+
+        // Esconder o formulário flutuante
+        floatingContainer.classList.remove("show");
+
+        // Atualizar a lista de tarefas
+        buscarTarefas();
+      } else {
+        throw new Error("Failed to create task. Status: " + response.status);
+      }
+    })
+    .catch((error) => {
+      console.error("Erro ao criar tarefa:", error);
+    });
+});
