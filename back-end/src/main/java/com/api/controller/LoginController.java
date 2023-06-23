0ff615e1afae6346d2282ec34d7f1754c82d62da
Matheus Soares <matheusmatias2622@gmail.com>
2023-06-23 19:49:11 -0300

    create user

--- back-end/src/main/java/com/api/controller/LoginController.java
+++ back-end/src/main/java/com/api/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ import io.jsonwebtoken.security.Keys;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin("*")
 public class LoginController {
 
 	
---
+++ front-end/js/createUser.js
@@ -0,0 +1,33 @@
+document.getElementById("signupModal").addEventListener("submit", function(e) {
+    e.preventDefault(); // Impede o envio do formulario
+
+    // Obtendo os valores dos campos input que serão enviados via post
+    let name = document.getElementById("signupName").value;
+    let username = document.getElementById("signupUsername").value;
+    let password = document.getElementById("signupPassword").value;
+    let confirmPassword = document.getElementById("signupConfirmPassword").value;
+
+    // Criando objeto com dados que serao enviados
+    let data= {
+        name: name,
+        username: username,
+        password: password,
+        confirmPassword: confirmPassword
+    }
+
+    axios
+        .post('http://localhost:8080/user/create', data)
+        .then(function (response) {
+
+            if(response.status === 200) {
+            console.log("Usuario criado com sucesso");
+            }else{
+                console.log("Ocorreu um erro na solicitação");
+            }
+
+        })
+        .catch(function(error){
+            console.error(error);
+            console.log("Erro na requisicao");
+        });
+})
